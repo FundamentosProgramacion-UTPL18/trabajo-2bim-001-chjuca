c3098b8efdf63efd1e1c66491b7f9be1b3abfb98
Carlos Juca <chjuca@utpl.edu.ec>
2018-05-31 10:32:30 -0500

    Ejercicio Semana9

--- Semana9/src/semana9/Ejercicio2.java
+++ Semana9/src/semana9/Ejercicio2.java
@@ -10,20 +10,20 @@ package semana9;
  * @author Carlos Juca
  */
 public class Ejercicio2 {
+
     public static void main(String[] args) {
-        int [] arreglo={1,2,3,4,5,6,7};
+        int[] arreglo = {1, 2, 3, 4, 5, 6, 7};
         for (int i = 0; i < arreglo.length; i++) {
-            if (arreglo[i]%2==0) {
-                System.out.printf("Tabla de Multiplicar de %d\n",arreglo[i]);
-                for (int j = 1; j <=12; j++) {
+            if (arreglo[i] % 2 == 0) {
+                System.out.printf("Tabla de Multiplicar de %d\n", arreglo[i]);
+                for (int j = 1; j <= 12; j++) {
                     int mult;
-                    mult=arreglo[i]*j;
-                    System.out.printf("%d*%d=%d\n",arreglo[i],arreglo[i],j,mult);
+                    mult = arreglo[i] * j;
+                    System.out.printf("%d*%d=%d\n", arreglo[i], j, mult);
                 }
             }
         }
-        
-        
+
     }
-    
+
 }
