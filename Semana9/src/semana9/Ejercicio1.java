9aed4f4cee7b380452d12014b017b2c9dadc8ff5
Carlos Juca <chjuca@utpl.edu.ec>
2018-05-31 10:12:19 -0500

    Ejercicio1 Semana9

---
+++ Semana9/src/semana9/Ejercicio1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semana9;
+
+/**
+ *
+ * @author Carlos Juca
+ */
+public class Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int c[]=new int [4];
+        c[0]=10;
+        for (int contador = 0; contador < c.length; contador++) {
+            System.out.println(c[contador]);
+        }
+    }
+    
+}
